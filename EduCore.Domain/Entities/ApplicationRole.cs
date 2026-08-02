a4695ac1012fa2586d211608c9d34fae2ed0bcb2
Pooya <pooya.dev01@gmail.com>
2026-08-02 20:34:56 +0330

    feat: add application role entity

---
+++ EduCore.Domain/Entities/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduCore.Domain.Entities;
+
+public class ApplicationRole : IdentityRole<int>
+{
+}
+
